a8f182103bd596c9dbacd903cb6d7f02a16db831
Asik8 <motasembillah84@gmail.com>
2024-04-29 22:16:24 +0600

    p3 done

--- CF-219432T.c
+++ CF-219432T.c
@@ -4,20 +4,20 @@
 #include<ctype.h>
 #include<stdlib.h>
 #include<stdbool.h>
-
+ 
 #define lli long long int
 #define max_size 100000
-
+ 
 int compare(const void *a, const void *b) {
     return (*(int*)a - *(int*)b);
 }
-
+ 
 int main()
 {
     int t;
     scanf("%d",&t);
     int st=1,sp=t-1;
-
+ 
     while(t--)
     {
         for(int i=0;i<sp;i++)
@@ -28,6 +28,6 @@ int main()
         st+=2;
         sp--;
     }
-
+ 
     return 0;
 }
